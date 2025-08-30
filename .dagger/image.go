2c3601ed223fe9bda198e31c3b68d2dd20eefbdd
Louis Fradin <lerenn@users.noreply.github.com>
2025-08-30 22:15:45 +0200

    ci: fix release (#83)

--- .dagger/image.go
+++ .dagger/image.go
@@ -13,7 +13,6 @@ type ImageInfo struct {
 	Arch            string
 	BuildBaseImage  string
 	TargetBaseImage string
-	ExportImage     bool
 }
 
 var (
@@ -24,91 +23,78 @@ var (
 			Arch:            "386",
 			BuildBaseImage:  "golang:alpine",
 			TargetBaseImage: "alpine",
-			ExportImage:     true,
 		},
 		"linux/amd64": {
 			OS:              "linux",
 			Arch:            "amd64",
 			BuildBaseImage:  "golang:alpine",
 			TargetBaseImage: "alpine",
-			ExportImage:     true,
 		},
 		"linux/arm/v6": {
 			OS:              "linux",
 			Arch:            "arm/v6",
 			BuildBaseImage:  "golang:alpine",
 			TargetBaseImage: "alpine",
-			ExportImage:     true,
 		},
 		"linux/arm/v7": {
 			OS:              "linux",
 			Arch:            "arm/v7",
 			BuildBaseImage:  "golang:alpine",
 			TargetBaseImage: "alpine",
-			ExportImage:     true,
 		},
 		"linux/arm64/v8": {
 			OS:              "linux",
 			Arch:            "arm64/v8",
 			BuildBaseImage:  "golang:alpine",
 			TargetBaseImage: "alpine",
-			ExportImage:     true,
 		},
 		"linux/ppc64le": {
 			OS:              "linux",
 			Arch:            "ppc64le",
 			BuildBaseImage:  "golang:alpine",
 			TargetBaseImage: "alpine",
-			ExportImage:     true,
 		},
 		"linux/riscv64": {
 			OS:              "linux",
 			Arch:            "riscv64",
 			BuildBaseImage:  "golang:alpine",
 			TargetBaseImage: "alpine",
-			ExportImage:     true,
 		},
 		"linux/s390x": {
 			OS:              "linux",
 			Arch:            "s390x",
 			BuildBaseImage:  "golang:alpine",
 			TargetBaseImage: "alpine",
-			ExportImage:     true,
 		},
 		"darwin/amd64": {
 			OS:              "darwin",
 			Arch:            "amd64",
 			BuildBaseImage:  "golang:alpine",
 			TargetBaseImage: "alpine",
-			ExportImage:     false,
 		},
 		"darwin/386": {
 			OS:              "darwin",
 			Arch:            "386",
 			BuildBaseImage:  "golang:alpine",
 			TargetBaseImage: "alpine",
-			ExportImage:     false,
 		},
 		"darwin/arm64": {
 			OS:              "darwin",
 			Arch:            "arm64",
 			BuildBaseImage:  "golang:alpine",
 			TargetBaseImage: "alpine",
-			ExportImage:     false,
 		},
 		"windows/386": {
 			OS:              "windows",
 			Arch:            "386",
 			BuildBaseImage:  "golang:alpine",
 			TargetBaseImage: "alpine",
-			ExportImage:     false,
 		},
 		"windows/amd64": {
 			OS:              "windows",
 			Arch:            "amd64",
 			BuildBaseImage:  "golang:alpine",
 			TargetBaseImage: "alpine",
-			ExportImage:     false,
 		},
 	}
 )
@@ -117,28 +103,35 @@ func AvailablePlatforms() []string {
 	return slices.Collect(maps.Keys(GoImageInfo))
 }
 
-// Image returns a container running the code-manager.
-func Image(
+// BuildImage returns a container for building binaries with cross-compilation.
+func BuildImage(
 	sourceDir *dagger.Directory,
 	runnerInfo ImageInfo,
 ) *dagger.Container {
-	// Determine target platform
-	targetPlatform := "linux/amd64" // default local platform
-	if runnerInfo.ExportImage {
-		targetPlatform = runnerInfo.OS + "/" + runnerInfo.Arch
-	}
-
 	buildOpts := dagger.DirectoryDockerBuildOpts{
 		BuildArgs: []dagger.BuildArg{
-			{Name: "BUILDPLATFORM", Value: runtime.GOOS + "/" + runtime.GOARCH},
 			{Name: "TARGETOS", Value: runnerInfo.OS},
 			{Name: "TARGETARCH", Value: runnerInfo.Arch},
-			{Name: "BUILDBASEIMAGE", Value: runnerInfo.BuildBaseImage},
-			{Name: "TARGETBASEIMAGE", Value: runnerInfo.TargetBaseImage},
-			{Name: "TARGETPLATFORM", Value: targetPlatform},
 		},
-		Dockerfile: "build/container/Dockerfile",
+		Dockerfile: "build/container/Dockerfile.build",
 	}
 
 	return sourceDir.DockerBuild(buildOpts)
+}
+
+// RuntimeImage returns a container for running (only for compatible platforms).
+func RuntimeImage(
+	buildContainer *dagger.Container,
+	runnerInfo ImageInfo,
+) *dagger.Container {
+	// Only create runtime image if the target OS matches the current build environment
+	// This ensures we only create Docker images for platforms we can actually run
+	if runnerInfo.OS != runtime.GOOS {
+		return nil
+	}
+
+	return dag.Container().
+		From(runnerInfo.TargetBaseImage).
+		WithFile("/usr/local/bin/cm", buildContainer.File("/go/bin/cm")).
+		WithEntrypoint([]string{"/usr/local/bin/cm"})
 }
